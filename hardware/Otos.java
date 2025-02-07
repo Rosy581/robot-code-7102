8d8461db9b17552a33a9fd05b67fee3db4ae4d70
Rosy <65262966+Rosy581@users.noreply.github.com>
2025-02-07 20:10:33 +0000

    ughhhhhhhhhhhhh

--- hardware/Otos.java
+++ hardware/Otos.java
@@ -3,24 +3,27 @@ package org.firstinspires.ftc.teamcode.hardware;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Otos {
     SparkFunOTOS myOtos;
     SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, -8.5, 0);
-    public void config(double x, double y){
+    public void config(double x, double y, HardwareMap hardwareMap){
+
+        SparkFunOTOS.Pose2D startingPos = new SparkFunOTOS.Pose2D(x, y, 0);
+        myOtos = hardwareMap.get(SparkFunOTOS.class, "otos");
+
         myOtos.setLinearUnit(DistanceUnit.INCH);
         myOtos.setAngularUnit(AngleUnit.DEGREES);
 
         myOtos.setOffset(offset);
 
         myOtos.setLinearScalar(1.03125);
-        myOtos.setAngularScalar(1.0);
+        myOtos.setAngularScalar(1.00416666667);
        
         myOtos.calibrateImu();
         
         myOtos.resetTracking();
-
-        SparkFunOTOS.Pose2D startingPos = new SparkFunOTOS.Pose2D(x, y, 0);
 
         myOtos.setPosition(startingPos);
     }
